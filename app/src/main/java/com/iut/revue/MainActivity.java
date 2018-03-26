ecb1df93d5e117bdc1151fcc5392aff4071072a2
marj57 <marj57@hotmail.fr>
2018-03-26 12:33:35 +0200

    Initial commit

---
+++ app/src/main/java/com/iut/revue/MainActivity.java
@@ -0,0 +1,32 @@
+package com.iut.revue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText mail, motDePasse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.bjaune); // j'initialise les widgets
+        mail = findViewById(R.id.etemail);
+        motDePasse = findViewById(R.id.etmot);
+
+    }
+
+    public void myButton(View view) {
+        String toast = String.format(getString(R.string.text)) + mail.getText().toString();
+        Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_LONG).show();
+    }
+
+
+}
